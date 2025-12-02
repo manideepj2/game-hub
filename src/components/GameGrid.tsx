283af3ec60a7878c145081e44e9dc4edd90312d2
Manideep Jammigumpula <manideepj2@gmail.com>
2025-12-02 16:28:08 -0500

    cleanup GameCard

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,7 +15,11 @@ const GameGrid = ({ gameQuery }: GameGridProps) => {
   const skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <>
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} padding="10px" spacing={3}>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        padding="10px"
+        spacing={6}
+      >
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer key={skeleton}>
